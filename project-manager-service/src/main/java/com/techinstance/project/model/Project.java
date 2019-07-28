0f22682bbf671a8a48b6c64fa675a97458a74059
arunds <arunds.mail@gmail.com>
2019-07-29 01:32:15 +0200

    ui integration testing

--- project-manager-service/src/main/java/com/techinstance/project/model/Project.java
+++ project-manager-service/src/main/java/com/techinstance/project/model/Project.java
@@ -1,17 +1,22 @@
 package com.techinstance.project.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,7 +27,7 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode
 @JsonDeserialize(as = Project.class)
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "tasks"})
 public class Project {
 
 	@Id
@@ -35,6 +40,12 @@ public class Project {
     private int priority;
     private String startDate;
     private String endDate;
+    @OneToMany(mappedBy = "project")
+    private Set<Task> tasks = new HashSet<>();
+    @Transient
+    private int noOfTasks;
+    @Transient
+    private int completed;
 
     public Project(User manager, String title, int priority, String startDate, String endDate) {
         this.manager = manager;
@@ -43,8 +54,22 @@ public class Project {
         this.startDate = startDate;
         this.endDate = endDate;
     }
-
+    
     public Project() {
     }
 
+	public void setNoOfTasks(int noOfTasks) {
+		this.noOfTasks = getTasks().size();
+	}
+
+	public void setCompleted(int completed) {
+		int count = 0;
+		for (Task task : tasks) {
+			if (task.getStatus() == "ENDED") {
+				count ++;
+			}
+		}
+		this.completed = count;
+	}
+    
 }
